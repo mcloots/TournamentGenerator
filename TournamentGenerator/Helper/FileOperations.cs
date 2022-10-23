d6a8f707aeba2d07d6a6d5d231e98feaf76f9637
Michaël Cloots <michael.cloots@thomasmore.be>
2022-10-23 21:43:17 +0200

    Done

---
+++ TournamentGenerator/Helper/FileOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentGenerator.Models;
+
+namespace TournamentGenerator.Helper
+{
+    public static class FileOperations
+    {
+        public static List<Participant> GetParticipants()
+        {
+            List<Participant> participants = new List<Participant>();
+            List<string> data;
+            Participant participant;
+            try
+            {
+                using (StreamReader reader = new StreamReader("DB/DB.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        data = reader.ReadLine().Split(';').ToList();
+                            participant = new Participant() { FirstName = data[0], LastName = data[1] };
+                            if (!participants.Contains(participant))
+                            {
+                            participants.Add(participant);
+                            }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return participants;
+        }
+    }
+}
--- TournamentGenerator/MainWindow.xaml.cs
+++ TournamentGenerator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TournamentGenerator.Helper;
+using TournamentGenerator.Models;
 
 namespace TournamentGenerator
 {
@@ -20,9 +23,157 @@ namespace TournamentGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Participant> participants = new List<Participant>();
+        Dictionary<string, ListBox> poulesDictionary = new Dictionary<string, ListBox>();
+        int _numberOfPoules = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            GetData();
+            SetComponents();
+        }
+
+        private void GetData()
+        {
+            participants = FileOperations.GetParticipants();
+        }
+
+        private void SetComponents()
+        {
+            lbParticipants.ItemsSource = participants;
+        }
+
+        private void btnGeneratePoules_Click(object sender, RoutedEventArgs e)
+        {
+            //First without animations
+            int numberOfPoules = 0;
+            if (int.TryParse(tbPoulesAmount.Text, out numberOfPoules))
+            {
+                if (numberOfPoules <= 12)
+                {
+                    _numberOfPoules = numberOfPoules;
+                    Reset();
+                    CreatePouleControls(numberOfPoules);
+                    GeneratePoules(numberOfPoules);
+                }
+                else
+                {
+                    MessageBox.Show("You can only create a maximum of 12 poules!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number!");
+            }
+        }
+
+        private void CreatePouleControls(int numberOfPoules)
+        {
+            for (int i = 1; i <= numberOfPoules; i++)
+            {
+                ListBox lb = new ListBox();
+                lb.Width = 150;
+                lb.Height = 300;
+                lb.Name = $"lb{i}";
+
+
+                switch (i)
+                {
+                    case 1:
+                        poulesDictionary.Add("A", lb);
+                        break;
+                    case 2:
+                        poulesDictionary.Add("B", lb);
+                        break;
+                    case 3:
+                        poulesDictionary.Add("C", lb);
+                        break;
+                    case 4:
+                        poulesDictionary.Add("D", lb);
+                        break;
+                    case 5:
+                        poulesDictionary.Add("E", lb);
+                        break;
+                    case 6:
+                        poulesDictionary.Add("F", lb);
+                        break;
+                    case 7:
+                        poulesDictionary.Add("G", lb);
+                        break;
+                    case 8:
+                        poulesDictionary.Add("H", lb);
+                        break;
+                    case 9:
+                        poulesDictionary.Add("I", lb);
+                        break;
+                    case 10:
+                        poulesDictionary.Add("J", lb);
+                        break;
+                    case 11:
+                        poulesDictionary.Add("K", lb);
+                        break;
+                    case 12:
+                        poulesDictionary.Add("L", lb);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            int counter = 0;
+            // Create stackpanel with label and listbox and add it to 'stackPoules'
+            foreach (var poule in poulesDictionary)
+            {
+                counter++;
+                Label lbl = new Label();
+                lbl.Content = poule.Key;
+
+                StackPanel pnl = new StackPanel();
+                pnl.Orientation = Orientation.Vertical;
+
+                pnl.Children.Add(lbl);
+                pnl.Children.Add(poule.Value);
+                pnl.Margin = new Thickness(10);
+
+                if (counter <= 6)
+                {
+                    stackPoules1.Children.Add(pnl);
+                }
+                else
+                {
+                    stackPoules2.Children.Add(pnl);
+                }
+            }
+        }
+
+        private async void GeneratePoules(int numberOfPoules)
+        {
+            participants.Shuffle();
+            int pouleCounter = 0;
+            foreach (var participant in participants)
+            {
+                //delay
+                await Task.Delay(2000);
+
+                //move over the poules
+                if (pouleCounter == numberOfPoules)
+                {
+                    pouleCounter = 0;
+                }
+
+                poulesDictionary.ElementAt(pouleCounter).Value.Items.Add(participant);
+
+                pouleCounter++;
+
+            }
+        }
+
+        private void Reset()
+        {
+            poulesDictionary = new Dictionary<string, ListBox>();
+            stackPoules1.Children.Clear();
+            stackPoules2.Children.Clear();
         }
     }
 }
